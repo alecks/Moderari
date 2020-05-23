b55b4d31021ece660d991fadcf219c78135f2853
Alex P <alexeek@protonmail.com>
2020-05-23 13:42:22 +0100

    chore: bump disgord

--- go.mod
+++ go.mod
@@ -3,12 +3,22 @@ module moderari
 go 1.14
 
 require (
-	github.com/andersfylling/disgord v0.17.0
-	github.com/auttaja/gommand v0.0.0-20200520185645-38f637de8da5
+	github.com/andersfylling/disgord v0.17.1
+	github.com/auttaja/gommand v0.0.0-20200523004444-4309e093c18b
 	github.com/fjah/gotcha v0.0.0-20200523114547-e72f149b9dd3
 	github.com/getsentry/sentry-go v0.6.1
 	github.com/go-redis/redis v6.15.8+incompatible
 	github.com/google/uuid v1.1.1
+	github.com/klauspost/compress v1.10.6 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
+	go.uber.org/zap v1.13.0 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	golang.org/x/tools v0.0.0-20200522201501-cb1345f3a375 // indirect
 	gopkg.in/yaml.v2 v2.3.0
+	nhooyr.io/websocket v1.8.6 // indirect
 )
--- internal/commands/config.go
+++ internal/commands/config.go
@@ -19,55 +19,63 @@ func configCmd(ctx *gommand.Context) error {
 	menu := gommand.NewEmbedMenu(embeds.Info("Configuration", "", ""), ctx)
 
 	prefixMenu := menu.NewChildMenu(
-		embeds.Info(
-			"Prefix",
-			"Enter a new prefix.",
-			"",
-		),
-		gommand.MenuButton{
-			Emoji:       "🔠",
-			Name:        "Prefix",
-			Description: "The prefix changes how you execute commands. If it's `|`, you'd use `|ping`.",
+		&gommand.ChildMenuOptions{
+			Embed: embeds.Info(
+				"Prefix",
+				"Enter a new prefix.",
+				"",
+			),
+			Button: &gommand.MenuButton{
+				Emoji:       "🔠",
+				Name:        "Prefix",
+				Description: "The prefix changes how you execute commands. If it's `|`, you'd use `|ping`.",
+			},
 		},
 	)
 	{
 		prefixMenu.AddBackButton()
 		prefixMenu.NewChildMenu(
-			embeds.Info(
-				"User Prefix",
-				"Enter a new prefix.",
-				"",
-			),
-			gommand.MenuButton{
-				Emoji:       "😛",
-				Name:        "User",
-				Description: "This changes the prefix for *you*, not the entire server. It'll apply everywhere.",
+			&gommand.ChildMenuOptions{
+				Embed: embeds.Info(
+					"User Prefix",
+					"Enter a new prefix.",
+					"",
+				),
+				Button: &gommand.MenuButton{
+					Emoji:       "😛",
+					Name:        "User",
+					Description: "This changes the prefix for *you*, not the entire server. It'll apply everywhere.",
+				},
 			},
 		).AddBackButton()
 		prefixMenu.NewChildMenu(
-			embeds.Info(
-				"Server Prefix",
-				"Enter a new prefix.",
-				"",
-			),
-			gommand.MenuButton{
-				Emoji:       "👨‍👩‍👧‍👦",
-				Name:        "Server",
-				Description: "This changes the prefix for everyone in the server. It'll only apply here.",
+			&gommand.ChildMenuOptions{
+				Embed: embeds.Info(
+					"Server Prefix",
+					"Enter a new prefix.",
+					"",
+				),
+				Button: &gommand.MenuButton{
+					Emoji:       "👨‍👩‍👧‍👦",
+					Name:        "Server",
+					Description: "This changes the prefix for everyone in the server. It'll only apply here.",
+				},
 			},
 		).AddBackButton()
 	}
 
 	menu.NewChildMenu(
-		embeds.Info(
-			"Ban Threshold",
-			"Enter a new ban threshold.",
-			"",
-		),
-		gommand.MenuButton{
-			Emoji:       "🔨",
-			Name:        "Ban Threshold",
-			Description: "The ban threshold is the amount of warnings at which a member is banned.",
+		&gommand.ChildMenuOptions{
+			Embed: embeds.Info(
+				"Ban Threshold",
+				"Enter a new ban threshold.",
+				"",
+			),
+			Button: &gommand.MenuButton{
+				Emoji:       "🔨",
+				Name:        "Ban Threshold",
+				Description: "The ban threshold is the amount of warnings at which a member is banned.",
+			},
 		},
 	).AddBackButton()
 
