2378e44b68477d10f521141db1e936cd6f299401
Alex P <alexeek@protonmail.com>
2020-05-24 07:50:35 +0100

    fix(golint): add comment

--- internal/events/init.go
+++ internal/events/init.go
@@ -4,6 +4,7 @@ import "github.com/andersfylling/disgord"
 
 var events = map[string]interface{}{}
 
+// Register registers all events in the `events` slice.
 func Register(client *disgord.Client) {
 	for i, v := range events {
 		client.On(i, v)
